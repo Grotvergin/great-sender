009b6c3bbfecb384bccbfd1b12c6ffbe544fa121
Roma <mishenin_r@mail.ry>
2024-04-22 12:27:40 +0300

    aDSFHGN

--- main.py
+++ main.py
@@ -14,14 +14,21 @@ def Main() -> None:
 
 def AnnualCheck():
     cur_time = datetime.now().strftime('%d.%m.%Y %H:%M')
-    data = GetSector('A2', 'G100', SERVICE, 'Расписание', SHEET_ID)
+    data = GetSector('A2', 'I100', SERVICE, 'Расписание', SHEET_ID)
     for row in data:
-        if cur_time == row[0] + ' ' + row[1]:
-            ids = row[5].split(',')
-            link = 'https://drive.google.com/uc?export=view&id=' + re.search(PATTERN, row[3]).group(1)
+        # if cur_time == row[0] + ' ' + row[1]:
+            ids = row[8].split(',')
             Stamp(f'Sending message to {' '.join(ids)}', 'i')
+            base = 'https://drive.google.com/uc?export=view&id='
+            media = []
+            for i in range(3, 7):
+                link = base + re.search(PATTERN, row[i]).group(1) if row[i] != '-' else None
+                media.append(link)
             for id_channel in ids:
-                BOT.send_photo(id_channel, link, caption=row[4])
+                if media[0]:
+                    BOT.send_photo(id_channel, media[0], caption=row[7])
+                if media[2]:
+                    BOT.send_video_note(id_channel, media[2])
 
 
 if __name__ == '__main__':
